33135f2555ae6a4e79a7d78ceab8993ce9fff27c
Noob-Ovi <abdullah.ovi098@gmail.com>
2023-11-05 18:06:58 +0600

    Setup initial installations and connection with mongodb

---
+++ index.js
@@ -0,0 +1,41 @@
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const jwt = require('jsonwebtoken')
+const { MongoClient, ServerApiVersion ,ObjectId} = require('mongodb');
+const port = process.env.PORT || 5000
+require('dotenv').config()
+app.use(cors())
+app.use(express.json())
+
+// configuring mongodb connection:
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.wukhoja.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    await client.connect();
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+
+app.get('/',(req,res)=>{
+    res.send('Server is running')
+})
+app.listen(port,()=>{
+    console.log('Server is running on',port);
+})
